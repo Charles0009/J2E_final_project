368e5a4f4e138cde7b3955c4ca471c539d85cdab
Charles0009 <charles.laverdure02@gmail.com>
2022-01-27 17:15:20 +0100

    web pages with css added

--- src/main/java/tsi/ensg/jee/servingwebcontent/GreetingController.java
+++ src/main/java/tsi/ensg/jee/servingwebcontent/GreetingController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tsi.ensg.jee.hibernate.colloque.Evenement.metier.Evenement;
+import tsi.ensg.jee.hibernate.colloque.Evenement.services.EvenementService;
+import tsi.ensg.jee.hibernate.colloque.Participant.metier.Participant;
 
 @Controller
 public class GreetingController {
@@ -19,6 +23,19 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "addEvent";
     }
+    @GetMapping("/addParticipant")
+    public String greeting3(@RequestParam(name="name", required=false, defaultValue="Charles") String name, Model model) {
+        model.addAttribute("name", name);
+        return "addParticipant";
+    }
+
+//    @GetMapping("/addParticipant")
+//    public String addParticipant(Model model) {
+//        model.addAttribute("participant", new Participant());
+//        //récupération avec Hibernate
+//        model.addAttribute("events", EvenementService.findAll());
+//        return "addParticipant";
+//    }
 
 //    @GetMapping("/greeting")
 //    public String greeting2(@RequestParam(name="name2", required=false, defaultValue="Charles") String name, Model model) {
